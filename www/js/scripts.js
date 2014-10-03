9c14df75cae37121f64fcbbe494c6ccc4f84ba56
PavelCSS <yashin.ziby@gmail.com>
2014-10-03 19:19:12 +0300

    Update

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,21 +1,18 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var pageTmpl =
+    '{{#header}}' +
+        '<header>' +
+            '<button id="back" class="btn fl-left icon-arrow-left">Back</button>{{{header.code}}}' +
+        '</header>' +
+    '{{/header}}' +
+    '{{#main}}' +
+        '<main>{{{main.code}}}</main>' +
+    '{{/main}}' +
+    '{{#footer}}' +
+        '<footer>{{{footer.code}}}</footer>' +
+    '{{/footer}}';
+
+var homeTmpl;
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,5 +30,24 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        homeTmpl = $('section').html();
     }
 };
+$('body').on('tap', '#gallery', function () {
+    var page_data, rendered;
+    page_data = {
+        'header': {
+            'code': 'Header title'
+        },
+        'main' :{
+            'code': 'Content page'
+        },
+        'footer   ': false
+    };
+    rendered = Mustache.to_html(pageTmpl, page_data);
+    $('section').html(rendered);
+})
+.on('tap', '#back', function () {
+    console.log(homeTmpl);
+    $('section').html(homeTmpl);
+});
