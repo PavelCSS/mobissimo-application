8b45337fde8ff4f976126b9e0cd930d209d77c3b
Pavel Yashin <yashin.ziby@gmail.com>
2014-10-05 15:23:31 +0300

    Add gallery

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -27,10 +27,73 @@ $('body').on('tap', '#gallery', function () {
     var gallery_data = {
         'page-name' : 'gallery',
         'header': {
-            'code': 'Header title'
+            'code': 'Mobbisimo gallery'
         },
         'main' :{
-            'code': 'Content page'
+            'code': function(){
+                var gall_list = {
+                    'items' : [
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        },
+                        {
+                            url : 'images/siq-2986_1280.jpg'
+                        }
+                    ]
+                };
+                parseTemplate(gall_list, '_gallery list.htm', 'main', false);
+            }
         },
         'footer   ': false
     };
@@ -40,18 +103,18 @@ $('body').on('tap', '#gallery', function () {
             'page-name' : 'upload',
             'header': false,
             'main' :{
-                'class' : 'gray-lighter-bg',
-                'code': '<img id="preview" class="polaroid-images" src="images/upload-image.png">'
+                'class' : '',
+                'code': '<img id="preview" class="img-polaroid" src="images/upload-image.png">'
             },
             'footer': {
                 'class' : 't-column_2 m-column_2 text-center mt30px mb30px',
                 'code':
                     '<span>' +
-                        '<button id="take-photo" class="btn fs48 rounded icon icon-pictures3 outline-bg">Gallery</button>' +
+                        '<button id="take-photo" class="btn fs48 rounded icon icon-camera outline-bg">Gallery</button>' +
                         '<span class="block">Take photo</span>' +
                     '</span>' +
                     '<span>' +
-                        '<button id="select-photo" class="btn fs48 rounded icon icon-folder-upload2 outline-bg">Upload</button>' +
+                        '<button id="select-photo" class="btn fs48 rounded icon icon-pictures3 outline-bg">Upload</button>' +
                         '<span class="block">Select photo</span>' +
                     '</span>'
             }
@@ -59,8 +122,9 @@ $('body').on('tap', '#gallery', function () {
     parseTemplate(upload_data, '_page.htm');
 }).on('click', '#take-photo', function(){
     navigator.camera.getPicture(onSuccess, onFail, {
-        quality         : 50,
-        destinationType : Camera.DestinationType.DATA_URL
+        quality         : 80,
+        destinationType : Camera.DestinationType.DATA_URL,
+        saveToPhotoAlbum: true
     });
 
     function onSuccess(imageData) {
@@ -73,13 +137,22 @@ $('body').on('tap', '#gallery', function () {
     }
 });
 
-function parseTemplate(json, tmpl){
+function parseTemplate(json, tmpl, selector, replace){
+    selector = (typeof selector == 'undefined') ? 'section' : selector;
+    replace = (typeof replace == 'undefined') ? true : replace;
     $.get(tmpl, function(response){
         if(json){
             var html = Mustache.to_html(response, json);
-            $('section').replaceWith(html);
+            insertTemplate(html, selector, replace);
         }else{
-            $('section').replaceWith(response);
+            insertTemplate(response, selector, replace);
         }
     });
+}
+function insertTemplate(html, selector, replace){
+    if(replace){
+        $(selector).replaceWith(html);
+    }else{
+        $(selector).html(html);
+    }
 }
